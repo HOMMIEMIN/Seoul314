aa7729a9ae60fe626c26ad18f2b2974d57f82586
HoMin <rlaghals999@naver.com>
2018-08-30 01:52:20 +0900

    초기 작업

---
+++ app/src/main/java/com/seoul314/seoul314/WithFragment.java
@@ -0,0 +1,29 @@
+package com.seoul314.seoul314;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WithFragment extends Fragment {
+
+
+    public WithFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_with, container, false);
+    }
+
+}
